7d5764566f98382a4e0a5179b090fb1dfd578f38
CMinusMinus <geocolson@gmail.com>
2015-10-10 08:25:41 -0700

    Rid us of the extra server and stuff

--- app/routes.js
+++ app/routes.js
@@ -3,20 +3,12 @@ var Restaurant = require('./models/restaurant');
 module.exports = function(app) {
     
     //back end
-    app.get('restaurants', function(req,res) { //find all the restaurants
-        
-        Restaurant.find(function(err, restaurants) {
-            if (err)
-                res.send(err)
-            res.json(restaurants);
-        });
-    });
     
     
     
     
     //front end
-    app.get('*', function(res,req) {
+    app.get('*', function(req,res) {
        res.sendfile('./public/views/index.html'); 
     });
 };
--- server.js
+++ server.js
@@ -1,61 +1,48 @@
-<<<<<<< HEAD
-//modules
-var express = require('express');
-var mongoose = require('mongoose');
-var app = express();
-var bodyParser = require('body-parser');
+// server.js
+
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
-//config files
+// configuration ===========================================
+    
+// config files
 var db = require('./config/db');
 
-//port
-var port = process.env.PORT || 3000;
+// set our port
+var port = process.env.PORT || 8080; 
 
-//connect to mongDB database
-mongoose.connect(db.url);
+// connect to our mongoDB database 
+// (uncomment after you enter in your own credentials in config/db.js)
+// mongoose.connect(db.url); 
 
-//parse json
-app.use(bodyParser.json());
+// get all data/stuff of the body (POST) parameters
+// parse application/json 
+app.use(bodyParser.json()); 
 
-//parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
-//parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-//override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/public'));
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override')); 
 
-//routes
-require('./app/routes')(app); //configure roots
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 
 
-//start app
-app.listen(port); //check port
-console.log('Magic happens on port '+port);
+// routes ==================================================
+require('./app/routes')(app); // configure our routes
 
-//expose app
-exports = module.exports = app;
-=======
-// This is just a sample code to test. We are not gonna use exact code below.
+// start app ===============================================
+// startup our app at http://localhost:8080
+app.listen(port);               
 
-//Lets require/import the HTTP module
-var http = require('http');
+// shoutout to the user                     
+console.log('Magic happens on port ' + port);
 
-//Lets define a port we want to listen to
-const PORT=8080; 
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
->>>>>>> 9228c991ef17ebf73830553c8e26aa0a031a9b22
+// expose app           
+exports = module.exports = app; 
